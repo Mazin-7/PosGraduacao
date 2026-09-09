9cd96227790091ee5b07a1cfbc3d7ba5a078f672
Gilmar <gilmar.devops@gmail.com>
2026-09-09 20:36:40 -0300

    Tipos de operadres

---
+++ Padroes-Web-Javascript/js/prog03.js
@@ -0,0 +1,12 @@
+let n = +"12";
+console.log(1, n, typeof n);
+
+let a = 3;
+console.log(2, a++ * 2, a);
+
+let nome = 'Gilmar';
+if(nome){
+    console.log(3, nome);
+} else {
+    console.log(4, 'Nome vazio');
+}
