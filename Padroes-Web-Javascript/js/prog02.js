3a4181da04c32742ad0eab6b5410fb68c97ea22a
Gilmar <gilmar.devops@gmail.com>
2026-09-09 20:14:04 -0300

    Tipos de dados

---
+++ Padroes-Web-Javascript/js/prog02.js
@@ -0,0 +1,26 @@
+let v;
+console.log(0, v, typeof v);
+
+v = 'JavaScript';
+console.log(1, v, typeof v);
+
+v = 31;
+console.log(2, v, typeof v);
+
+let nome = 'Gilmar';
+let mensagem = 
+    `<p><
+        Olá ${nome}
+    </p>`;
+console.log(3, mensagem, typeof mensagem);
+
+let a = 3, b = 5;
+console.log(4,
+` A soma de ${a} + ${b} é igual a ${a + b}`
+)
+
+v = null;
+console.log(5, v, typeof v);
+
+v = undefined;
+console.log(6, v, typeof v);
