b1ce0aeac612f18d27c6fc12ece1d9e35430c15a
Gilmar <gilmar.devops@gmail.com>
2026-09-14 20:44:30 -0300

    Objetos

--- Padroes-Web-Javascript/js/prog07.js
+++ Padroes-Web-Javascript/js/prog07.js
@@ -7,3 +7,64 @@ Introdução a objetos.
 - O valor e especificado por : e não por =
 
 */
+
+let musica = {};
+console.log(musica);
+
+musica.nome = "I'm Eighteen";
+musica.artista = 'Alice Cooper';
+musica.segundos = 180;
+
+console.log(musica);
+
+let joao = {
+    nome: 'João',
+    idade: 35,
+};
+
+let maria = {};
+for (let chave in joao) {
+    maria[chave] = joao[chave];
+}
+
+maria.nome = 'Maria';
+maria.sobrenome = 'Santos';
+
+console.log(joao);
+console.log(maria);
+
+function Usuario(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    this.maiorIdade = function () {
+        return this.idade >= 18;
+    };
+}
+
+function criaUsuario(nome, idade) {
+    return {
+        nome: nome,
+        idade: idade,
+        maiorIdade: function () {
+            return this.idade >= 18;
+        },
+    };
+}
+
+class User {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    maiorIdade() {
+        return this.idade >= 18;
+    }
+}
+
+/* let joao1 = criaUsuario('João', 35)*/
+//let joao1 = new  Usuario('joão', 35);
+
+let joao1 = new User('João', 15);
+
+console.log(joao1);
+console.log(joao1.maiorIdade());
