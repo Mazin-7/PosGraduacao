2374e57baccb9e39f22e93df38be8003fbd1150e
Gilmar <gilmar.devops@gmail.com>
2026-09-14 19:58:30 -0300

    Introdução a objetos

---
+++ Padroes-Web-Javascript/js/prog07.js
@@ -0,0 +1,9 @@
+/*
+
+Introdução a objetos.
+- São estruturas de nome e valor.
+- São delimitados por chave.
+- Os atibutos são separados por virgula e não por ponto e virgula.
+- O valor e especificado por : e não por =
+
+*/
