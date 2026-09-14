7313efe9cfc2ab08b34c3891c5c6ed45ad40b623
Gilmar <gilmar.devops@gmail.com>
2026-09-14 10:28:58 -0300

    Funções

---
+++ Padroes-Web-Javascript/js/prog05.js
@@ -0,0 +1,12 @@
+// Funções
+function somar(a, b) {
+    let resultado = a + b;
+    return resultado;
+}
+
+let nome = "João";
+function mensagem(nome) {
+    console.log("Olá, " + nome + "!");
+}
+
+mensagem('Ana');
